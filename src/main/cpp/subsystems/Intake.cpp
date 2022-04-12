01a4ed9b38bb23d354e4798992bb962db97a7bf6
Esteb37 <esteban37padilla@gmail.com>
2022-04-11 23:45:01 -0500

    Intake now inherits from solenoidsubsystem

--- src/main/cpp/subsystems/Intake.cpp
+++ src/main/cpp/subsystems/Intake.cpp
@@ -2,32 +2,23 @@
 
 using namespace frc;
 
-Intake::Intake()
+Intake::Intake(MotorConfig config, unsigned int port) : MotorSubsystem(config, port)
 {
+	SetName("Intake");
+}
+
+Intake::Intake(MotorConfig config, unsigned int port, unsigned int solenoidA, unsigned int solenoidB) : MotorSubsystem(config, port), SolenoidSubsystem(solenoidA, solenoidB)
+{
+	SetName("Intake");
+}
+
+Intake::Intake(MotorConfig config, unsigned int port, unsigned int rightA, unsigned int rightB, unsigned int leftA, unsigned int leftB) : MotorSubsystem(config, port), SolenoidSubsystem(rightA, rightB, leftA, leftB)
+{
+	SetName("Intake");
 }
 
 void Intake::Periodic()
 {
-}
-
-void Intake::SetMotor(double speed)
-{
-	m_motor.Set(speed * k_intakeMaxSpeed);
-}
-
-double Intake::GetMotor()
-{
-	return m_motor.Get();
-}
-
-void Intake::InvertMotor(bool inverted)
-{
-	m_motor.SetInverted(inverted);
-}
-
-void Intake::PrintMotor()
-{
-	SmartDashboard::PutNumber("Intake Motor", GetMotor());
 }
 
 void Intake::Take()
@@ -42,50 +33,10 @@ void Intake::Spit()
 
 void Intake::Lower()
 {
-	if (m_solenoidsInverted)
-	{
-		m_rightSolenoid.Set(DoubleSolenoid::Value::kForward);
-		m_leftSolenoid.Set(DoubleSolenoid::Value::kForward);
-	}
-	else
-	{
-		m_rightSolenoid.Set(DoubleSolenoid::Value::kReverse);
-		m_leftSolenoid.Set(DoubleSolenoid::Value::kReverse);
-	}
+	OpenSolenoids();
 }
 
 void Intake::Raise()
 {
-	if (!m_solenoidsInverted)
-	{
-		m_rightSolenoid.Set(DoubleSolenoid::Value::kForward);
-		m_leftSolenoid.Set(DoubleSolenoid::Value::kForward);
-	}
-	else
-	{
-		m_rightSolenoid.Set(DoubleSolenoid::Value::kReverse);
-		m_leftSolenoid.Set(DoubleSolenoid::Value::kReverse);
-	}
-}
-
-void Intake::SolenoidsOff()
-{
-	m_rightSolenoid.Set(DoubleSolenoid::Value::kOff);
-	m_leftSolenoid.Set(DoubleSolenoid::Value::kOff);
-}
-
-unsigned int Intake::GetRightSolenoid()
-{
-	return m_rightSolenoid.Get();
-}
-
-unsigned int Intake::GetLeftSolenoid()
-{
-	return m_leftSolenoid.Get();
-}
-
-void Intake::PrintSolenoids()
-{
-	SmartDashboard::PutNumber("Intake Right Solenoid", GetRightSolenoid());
-	SmartDashboard::PutNumber("Intake Left Solenoid", GetLeftSolenoid());
+	CloseSolenoids();
 }
--- src/main/include/subsystems/Intake.h
+++ src/main/include/subsystems/Intake.h
@@ -1,5 +1,7 @@
 #pragma once
 #include "Constants.h"
+#include "subsystems/MotorSubsystem.h"
+#include "subsystems/SolenoidSubsystem.h"
 #include <frc/DoubleSolenoid.h>
 #include <frc/motorcontrol/VictorSP.h>
 #include <frc/smartdashboard/SmartDashboard.h>
@@ -10,11 +12,15 @@ using namespace frc;
 using namespace frc2;
 using namespace rev;
 
-class Intake : public SubsystemBase
+class Intake : public MotorSubsystem, public SolenoidSubsystem
 {
 
 public:
-	Intake();
+	Intake(MotorConfig, unsigned int);
+
+	Intake(MotorConfig, unsigned int, unsigned int, unsigned int);
+
+	Intake(MotorConfig, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int);
 
 	void Periodic() override;
 
@@ -40,75 +46,5 @@ public:
 	 */
 	void Raise();
 
-	// ---------- Motor -----------
-
-	/**
-	 * @brief Sets the Motor speed
-	 * @param speed Speed and direction to turn
-	 */
-	void SetMotor(double);
-
-	/**
-	 * @brief Gets the Motor speed
-	 */
-	double GetMotor();
-
-	/**
-	 * @brief Invert motor direction
-	 * @param invert True to invert, false to not
-	 */
-	void InvertMotor(bool);
-
-	/**
-	 * @brief Publishes the motor's value on the SmartDashboard
-	 */
-	void PrintMotor();
-
-	// ---------- Solenoids -----------
-
-	/**
-	 * @brief Invert which value is up or down
-	 */
-	void InvertSolenoids();
-
-	/**
-	 * @brief Sets the solenoids to kOFF
-	 */
-	void SolenoidsOff();
-
-	/**
-	 * @brief Get the right solenoid state
-	 */
-	unsigned int GetRightSolenoid();
-
-	/**
-	 * @brief Get the left solenoid state
-	 */
-	unsigned int GetLeftSolenoid();
-
-	/**
-	 * @brief Publishes the solenoids' value on the SmartDashboard
-	 */
-	void PrintSolenoids();
-
 private:
-	// ---------- Motor ----------
-
-	VictorSP m_motor{pIntakeMotor};
-
-	// TODO : define motor type
-
-	// CANSparkMax m_motorSparkMax{pIntakeMotor, CANSparkMax::MotorType::kBrushed};
-
-	// VictorSPX m_motor{pIntakeMotor};
-
-	// ---------- Solenoids ---------
-
-	DoubleSolenoid m_rightSolenoid{PneumaticsModuleType::REVPH, pIntakeRightSolenoidForward, pIntakeRightSolenoidReverse};
-
-	DoubleSolenoid m_leftSolenoid{PneumaticsModuleType::REVPH, pIntakeLeftSolenoidForward, pIntakeLeftSolenoidReverse};
-
-	// ---------- Attributes ----------
-
-	bool m_solenoidsInverted = false;
 };
