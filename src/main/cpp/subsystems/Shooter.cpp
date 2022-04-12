2db2465927a3aaa32fb2347b28d0aabd24c7c710
Esteb37 <esteban37padilla@gmail.com>
2022-04-12 00:04:11 -0500

    Shooter is now encoder and solenoid subsystem

--- src/main/cpp/subsystems/Shooter.cpp
+++ src/main/cpp/subsystems/Shooter.cpp
@@ -1,9 +1,18 @@
 #include "subsystems/Shooter.h"
 
-Shooter::Shooter()
+Shooter::Shooter(MotorConfig mconfig, unsigned int mport) : EncoderSubsystem(mconfig, mport), MotorSubsystem(mconfig, mport)
 {
-	m_PIDController.SetTolerance(k_shooterPIDTolerance);
-	m_encoder.SetVelocityConversionFactor(k_shooterVCF);
+	SetName("Shooter");
+}
+
+Shooter::Shooter(MotorConfig mconfig, EncoderConfig econfig, unsigned int mport) : EncoderSubsystem(mconfig, econfig, mport), MotorSubsystem(mconfig, mport)
+{
+	SetName("Shooter");
+}
+
+Shooter::Shooter(MotorConfig mconfig, EncoderConfig econfig, unsigned int mport, unsigned int encoderA, unsigned int encoderB) : EncoderSubsystem(mconfig, econfig, mport, encoderA, encoderB), MotorSubsystem(mconfig, mport)
+{
+	SetName("Shooter");
 }
 
 void Shooter::Periodic()
@@ -13,83 +22,14 @@ void Shooter::Periodic()
 void Shooter::Reset()
 {
 	ResetEncoder();
+	ResetSpeedPID();
 	ResetServos();
 	SolenoidsOff();
-}
-
-void Shooter::SetMotor(double speed)
-{
-	m_motor.Set(speed * k_shooterMaxSpeed);
-}
-
-double Shooter::GetMotor()
-{
-	return m_motor.Get();
-}
-
-void Shooter::InvertMotor(bool inverted)
-{
-	m_motor.SetInverted(inverted);
-}
-
-void Shooter::PrintMotor()
-{
-	SmartDashboard::PutNumber("Shooter Motor", GetMotor());
-}
-
-double Shooter::GetEncoder()
-{
-	return m_encoder.GetVelocity();
-
-	// TODO : change if normal encoder
-	// return m_encoder.GetRate()
-}
-
-void Shooter::ResetEncoder()
-{
-	m_encoder.SetPosition(0);
-
-	// TODO : change if normal encoder
-	// m_encoder.Reset();
-}
-
-void Shooter::InvertEncoder(bool inverted)
-{
-	m_encoder.SetInverted(inverted);
-
-	// TODO : change if normal encoder
-	// m_encoder.SetReverseDirection(inverted);
-}
-
-void Shooter::PrintEncoder()
-{
-	SmartDashboard::PutNumber("Shooter Encoder", GetEncoder());
-}
-
-bool Shooter::ReachRPM(double rpm)
-{
-	m_PIDController.SetSetpoint(rpm);
-
-	double output = m_PIDController.Calculate(GetEncoder());
-
-	SetMotor(std::clamp(output, -k_shooterMaxSpeed, k_shooterMaxSpeed));
-
-	return m_PIDController.AtSetpoint();
 }
 
 double Shooter::CalculateRPM()
 {
 	return shooterRPMFromDistance(m_limelight.GetDistanceToTarget());
-}
-
-void Shooter::ResetPID()
-{
-	m_PIDController.Reset();
-}
-
-void Shooter::PrintPIDError()
-{
-	SmartDashboard::PutNumber("Shooter PID Error", m_PIDController.GetVelocityError());
 }
 
 void Shooter::SetAngleServos(double angle)
@@ -116,42 +56,11 @@ double Shooter::GetLeftServo()
 
 void Shooter::PrintServos()
 {
-	SmartDashboard::PutNumber("Shooter Right Servo", GetRightServo());
-	SmartDashboard::PutNumber("Shooter Left Servo", GetLeftServo());
+	SmartDashboard::PutNumber(GetName() + " Right Servo", GetRightServo());
+	SmartDashboard::PutNumber(GetName() + "Shooter Left Servo", GetLeftServo());
 }
 
-void Shooter::SetAngleSolenoids(bool forward)
+void Shooter::Shoot()
 {
-	if (forward ^ m_solenoidsInverted)
-	{
-		m_rightSolenoid.Set(DoubleSolenoid::Value::kForward);
-		m_leftSolenoid.Set(DoubleSolenoid::Value::kForward);
-	}
-	else
-	{
-		m_rightSolenoid.Set(DoubleSolenoid::Value::kReverse);
-		m_leftSolenoid.Set(DoubleSolenoid::Value::kReverse);
-	}
-}
-
-void Shooter::SolenoidsOff()
-{
-	m_rightSolenoid.Set(DoubleSolenoid::Value::kOff);
-	m_leftSolenoid.Set(DoubleSolenoid::Value::kOff);
-}
-
-int Shooter::GetRightSolenoid()
-{
-	return m_rightSolenoid.Get();
-}
-
-int Shooter::GetLeftSolenoid()
-{
-	return m_leftSolenoid.Get();
-}
-
-void Shooter::PrintSolenoids()
-{
-	SmartDashboard::PutBoolean("Shooter Right Solenoid", GetRightSolenoid());
-	SmartDashboard::PutBoolean("Shooter Left Solenoid", GetLeftSolenoid());
+	SetMotor(1);
 }
--- src/main/include/subsystems/Shooter.h
+++ src/main/include/subsystems/Shooter.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "Constants.h"
+#include "subsystems/EncoderSubsystem.h"
+#include "subsystems/SolenoidSubsystem.h"
 #include <any>
 #include <frc/DoubleSolenoid.h>
 #include <frc/Encoder.h>
 #include <frc/Servo.h>
+#include <frc/Timer.h>
 #include <frc/controller/PIDController.h>
 #include <frc/motorcontrol/VictorSP.h>
 #include <frc2/command/SubsystemBase.h>
@@ -15,94 +18,23 @@ using namespace frc;
 using namespace frc2;
 using namespace rev;
 
-class Shooter : public SubsystemBase
+class Shooter : public EncoderSubsystem, public SolenoidSubsystem
 {
 
 public:
-	Shooter();
+	Shooter(MotorConfig, unsigned int);
+
+	Shooter(MotorConfig, EncoderConfig, unsigned int);
+
+	Shooter(MotorConfig, EncoderConfig, unsigned int, unsigned int, unsigned int);
 
 	void Periodic() override;
 
-	// ---------- Actions -----------
-
-	/**
-	 * @brief Resets systems
-	 */
 	void Reset();
 
-	// --------- Motor ---------
+	// ---------- Actions -----------
 
-	/**
-	 * @brief Sets the Motor speed
-	 * @param speed Speed and direction to turn
-	 */
-	void SetMotor(double);
-
-	/**
-	 * @brief Gets the Motor speed
-	 */
-	double GetMotor();
-
-	/**
-	 * @brief Invert motor direction
-	 * @param invert True to invert, false to not
-	 */
-	void InvertMotor(bool);
-
-	/**
-	 * @brief Publishes the motor speed to the dashboard
-	 */
-	void PrintMotor();
-
-	// --------- Encoder ---------
-
-	/**
-	 * @brief Get the Encoder value
-	 * @return double
-	 */
-	double GetEncoder();
-
-	/**
-	 * @brief Sets the encoder's value to 0
-	 */
-	void ResetEncoder();
-
-	/**
-	 * @brief Inverts the encoder's direction
-	 * @param invert True to invert, false to not
-	 */
-	void InvertEncoder(bool);
-
-	/**
-	 * @brief Publishes the Encoder value to the dashboard
-	 */
-	void PrintEncoder();
-
-	// --------- PID ---------
-
-	/**
-	 * @brief Sets the shooter's motor to a specified RPM
-	 * @param rpm The RPM to set the motor to
-	 * @return If the motor has reached the RPM
-	 */
-	bool ReachRPM(double);
-
-	/**
-	 * @brief Calculates the required RPM depending on distance to target
-	 *
-	 * @return The required RPM
-	 */
-	double CalculateRPM();
-
-	/**
-	 * @brief Resets RPM PID
-	 */
-	void ResetPID();
-
-	/**
-	 * @brief Prints RPM PID error to the dashboard
-	 */
-	void PrintPIDError();
+	void Shoot();
 
 	// --------- Angle with Servos --------
 
@@ -134,76 +66,16 @@ public:
 	 */
 	void PrintServos();
 
-	// --------- Angle with Solenoids ---------
-
-	/**
-	 * @brief Sets the shooting angle by using solenoids
-	 * @param up True to set the shooter to up, false to set it to down
-	 */
-
-	void SetAngleSolenoids(bool);
-
-	/**
-	 * @brief Inverts which value is open and closed
-	 */
-	void InvertSolenoids();
-
-	/**
-	 * @brief Set the solenoids to kOff
-	 */
-	void SolenoidsOff();
-
-	/**
-	 * @brief Gets the state of the right solenoid
-	 */
-	int GetRightSolenoid();
-
-	/**
-	 * @brief Gets the state of the left solenoid
-	 */
-	int GetLeftSolenoid();
-
-	/**
-	 * @brief Prints the state of the solenoids to the dashboard
-	 */
-	void PrintSolenoids();
+	double CalculateRPM();
 
 private:
-	// --------- Motor ---------
-
-	CANSparkMax m_motor{pShooterMotor, CANSparkMax::MotorType::kBrushless};
-
-	// TODO : define motor type
-
-	// VictorSP m_motor{pShooterMotor};
-
-	// VictorSPX m_motor{pShooterMotor};
-
-	// --------- Encoder ---------
-
-	SparkMaxRelativeEncoder m_encoder{m_motor.GetEncoder()};
-
-	// TODO : define encoder type
-
-	//	Encoder m_encoder{k_shooterEncoderA, k_shooterEncoderB, false, Encoder::EncodingType::k4X};
-
-	PIDController m_PIDController{k_shooterP, k_shooterI, k_shooterD};
-
-	// -------- Angle actuators --------
-
-	DoubleSolenoid m_rightSolenoid{PneumaticsModuleType::REVPH, pShooterRightSolenoidForward, pShooterRightSolenoidReverse};
-
-	DoubleSolenoid m_leftSolenoid{PneumaticsModuleType::REVPH, pShooterLeftSolenoidForward, pShooterLeftSolenoidReverse};
-
 	Servo m_rightServo{pShooterRightServo};
 
 	Servo m_leftServo{pShooterLeftServo};
 
-	// --------- Sensors -------------
-
 	Limelight m_limelight;
 
-	// --------- Attributes -----------
+	Timer m_timer;
 
-	bool m_solenoidsInverted = false;
+	bool m_timerStarted;
 };
