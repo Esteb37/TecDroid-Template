18377539c66a205508821d208965b084a6b8b596
Esteb37 <esteban37padilla@gmail.com>
2022-04-22 19:02:14 -0500

    Wakatime

--- src/main/cpp/RobotContainer.cpp
+++ src/main/cpp/RobotContainer.cpp
@@ -17,73 +17,35 @@ double GetBool(string name, double alt)
 RobotContainer::RobotContainer()
 {
 
-	m_drivetrain.ResetMotors();
-	m_drivetrain.ResetSensors();
-	m_drivetrain.SetSafetyEnabled(true);
-	m_drivetrain.SetPositionConversionFactor(k_drivetrainDPR);
-	m_drivetrain.m_limelight.ConfigureMount(35, 72);
-	m_shooter.SetMaxSpeed(k_shooterMaxSpeed);
 	m_shooter.ConfigureServos(pwm_shooterRightServo, pwm_shooterLeftServo);
-	m_shooter.SetSpeedConversionFactor(k_shooterVCF);
-	m_shooter.ConfigureSpeedPID(k_shooterP, k_shooterI, k_shooterD, k_shooterPIDTolerance);
-	m_shooter.InvertEncoder(false);
-	m_shooter.InvertMotor(false);
+	m_shooter.m_limelight.ConfigureMount(72, 62);
 
-	m_intake.SetMaxSpeed(k_intakeMaxSpeed);
-	m_intake.InvertMotor(false);
-
-	m_feeder.SetMaxSpeed(k_feederMaxSpeed);
-	m_feeder.InvertMotor(false);
-
-	m_elevator.SetMaxSpeed(k_elevatorMaxSpeed);
-	m_elevator.ConfigureLimitSwitches(dio_elevatorLimitTop, dio_elevatorLimitBottom);
-	m_elevator.SetPositionConversionFactor(k_elevatorDPR);
-	m_elevator.ConfigurePositionPID(k_elevatorHeightP, k_elevatorHeightI, k_elevatorHeightD, k_elevatorHeightPIDTolerance);
-	m_elevator.SetHeightToFloor(30);
-	m_elevator.SetMinMaxHeight(0, 130);
-	m_elevator.SetLimitSafety(true);
-	m_elevator.SetPositionSafety(true);
-	m_elevator.InvertEncoder(false);
-	m_elevator.InvertMotors({false, false});
-
-	m_claw.InvertHand(false);
-	m_claw.InvertWrist(false);
-
-	m_turret.SetMaxSpeed(k_turretMaxSpeed);
-	m_turret.ConfigureLimitSwitches(dio_turretLimitRight, dio_turretLimitLeft);
-	m_turret.SetPositionConversionFactor(k_turretDPR);
-	m_turret.ConfigurePositionPID(k_turretAngleP, k_turretAngleI, k_turretAngleD, k_turretAnglePIDTolerance);
-	m_turret.ConfigureAlignPID(k_turretAlignP, k_turretAlignI, k_turretAlignD, k_turretAlignPIDTolerance);
-	m_turret.SetFreedom(360);
-	m_turret.SetLimitSafety(true);
-	m_turret.SetPositionSafety(true);
-	m_turret.InvertEncoder(false);
-	m_turret.InvertMotor(false);
+	m_shooter.SetEncoderPorts(dio_shooterEncoderA, dio_shooterEncoderB);
+	m_shooter.SetRPMConversionFactor(k_shooterVCF);
+	m_shooter.ConfigureSolenoids(0, 1, 2, 3);
 }
 
 void RobotContainer::TeleopInit()
 {
+	m_shooter.Reset();
+	m_shooter.ResetRPMPID();
+	m_shooter.ResetServos();
 }
 void RobotContainer::TeleopPeriodic()
 {
-
-	m_drivetrain.PrintGyro();
-	m_drivetrain.PrintMotors();
-	m_drivetrain.ConfigureAlignPID(k_alignP, k_alignI, k_alignD, k_alignPIDTolerance, GetBool("Invert", false));
-	m_drivetrain.ConfigureDistancePID(k_distanceP, k_distanceI, k_distanceD, k_distancePIDTolerance, GetBool("Invert", false));
-	m_drivetrain.ConfigureTurnPID(k_turnP, k_turnI, k_turnD, k_turnPIDTolerance, GetBool("Invert", false));
-	m_drivetrain.ConfigureMovePID(k_moveP, k_moveI, k_moveD, k_movePIDTolerance, GetBool("Invert", false));
-	/*m_drivetrain.Move(GetNumber();
-	m_drivetrain.PrintMoveError();*/
-
-	/*m_drivetrain.Turn(("Distance", 100), GetNumber("Speed", -1));
-	m_drivetrain.PrintTurnError();
-	m_drivetrain.MoveTo(GetNumber("X", 100), GetNumber("Y", 100), 1, 1);
-	m_drivetrain.PrintMoveToError();
-	m_drivetrain.PrintCurrentPosition();*/
-	// m_drivetrain.SetAngleWithTarget(10, 1);
-	/*m_drivetrain.AlignWithTarget(1);
-	m_drivetrain.PrintAlignError();*/
-	m_drivetrain.SetDistanceWithTarget(250, 100, 1);
-	m_drivetrain.PrintSetDistanceError();
+	m_shooter.SetMaxSpeed(GetNumber("Max Speed", k_shooterMaxSpeed));
+	m_shooter.SetMotor(GetNumber("Speed", 1));
+	m_shooter.InvertEncoder(GetBool("IE", false));
+	m_shooter.InvertMotor(GetBool("IM", false));
+	m_shooter.PrintRPM();
+	m_shooter.SetAngleServos(GetNumber("Servos", 1));
+	m_shooter.PrintServos();
+	m_shooter.InvertSolenoids(GetNumber("IS", false));
+	m_shooter.OpenSolenoids();
+	m_shooter.PrintSolenoids();
+	/*m_shooter.Shoot();
+	m_shooter.ConfigureRPMPID(k_shooterP, k_shooterI, k_shooterD, k_shooterPIDTolerance, GetBool("IP", false));
+	m_shooter.SetRPM(GetNumber("SetSpeed", 360), GetNumber("Speed", 1));
+	m_shooter.PrintRPMError();
+	SmartDashboard::PutNumber("RPM", m_shooter.CalculateRPM(250));*/
 }
